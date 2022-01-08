751b4ad86f592ef4466e3b7d9dbf6c5581e4a20a
Austin Keller <austinkllr@gmail.com>
2022-01-07 20:06:06 -0800

    finished contact page

--- src/contact.js
+++ src/contact.js
@@ -8,6 +8,7 @@ export default function createContact() {
 
     contactDiv.appendChild(createHeader());
     contactDiv.appendChild(createInfo());
+    contactDiv.appendChild(createSchedule());
     content.appendChild(contactDiv);
 
 }
@@ -22,19 +23,37 @@ const createHeader = () => {
 const createInfo = () => {
     const info = document.createElement('div');
     
-    const street = document.createElement('p');
+    const street = document.createElement('h4');
     street.textContent = "2000 Baker St,";
     
-    const cityState = document.createElement('p');
+    const cityState = document.createElement('h4');
     cityState.textContent = "San Francisco, CA 94123";
     
-    const phone = document.createElement('p');
+    const phone = document.createElement('h4');
     phone.textContent = "(925) 900-8000";
 
-    const email = document.createElement('p');
+    const email = document.createElement('h4');
     email.textContent = "restaurant@gmail.com";
 
     info.append(street, cityState, phone, email);
-
+    
     return info;
+}
+
+const createSchedule = () => {
+    const schedule = document.createElement('div');
+
+    const tueThur = document.createElement('p');
+    tueThur.textContent = "Tuesday-Thursday 5 PM - 9 PM";
+    
+    const friSat = document.createElement('p');
+    friSat.textContent = "Friday & Saturday 12 PM - 9:30 PM";
+    
+    const sunday = document.createElement('p');
+    sunday.textContent = "Sunday 12 PM - 9 PM";
+
+    schedule.append(tueThur, friSat, sunday);
+    schedule.classList.add('schedule');
+
+    return schedule
 }
