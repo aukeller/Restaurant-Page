a3cd4e0c409f56d2b44c311ad635b89e7d00d0e6
Austin Keller <austinkllr@gmail.com>
2022-01-07 20:50:20 -0800

    finshed menu page

--- src/menu.js
+++ src/menu.js
@@ -2,9 +2,27 @@ export default function createMenu() {
     const content = document.querySelector('#content');
 
     const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menu');
+    
+    const firstMenuItems = ["Marinated Olives", "Ignalat burrata", "smoked burrata", "salumi", "arugula salad", "citrus salad"];
+    const secondMenuItems = ["braised meatballs", "beet salad", "romanesco", "zuppa", "trippa", "Calamari", "octopus carpaccio"];
 
-    menuDiv.textContent = "menu";
+    menuDiv.appendChild(createMenuPanel(firstMenuItems));
+    menuDiv.appendChild(createMenuPanel(secondMenuItems));
 
     content.appendChild(menuDiv);
 
 }
+
+const createMenuPanel = (items) => {
+    const container = document.createElement('div');
+    
+    items.forEach(item => {
+        let itemP = document.createElement('h3');
+        itemP.textContent = item;
+        container.appendChild(itemP);
+    });
+
+    return container;
+}
+
